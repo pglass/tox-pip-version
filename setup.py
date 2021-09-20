b72ffdafd6555c850ef0fc979a4ac1b410059c74
aberges <armand.berges@soprasteria.com>
2021-09-20 08:36:44 +0200

    🐛 Change first files

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ long_description = open('README.md').read()
 import setuptools
 
 setuptools.setup(
-    name='tox-pip-version',
-    author='Paul Glass',
-    author_email='pnglass@gmail.com',
-    description='Select PIP version to use with tox',
+    name='tox-setuptools-version',
+    author='LvffY',
+    author_email='louberger@hotmail.fr',
+    description='Select SETUPTOOLS version to use with tox',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/pglass/tox-pip-version',
     license='MIT',
-    version='0.0.7',
+    version='0.0.0.1',
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['tox>=2.0'],
@@ -35,5 +35,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
